6e3eb26d46e390db0f5866e0fc57e07b855c185e
fredp613 <fredp613@gmail.com>
2015-01-25 18:06:19 -0500

    initial commit

---
+++ delegator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delegator
+//
+//  Created by Fred Pearson on 2015-01-25.
+//  Copyright (c) 2015 Frederick Pearson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
